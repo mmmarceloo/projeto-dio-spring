e8d2b099909fca5b5806553ebfa8f39602aa36c0
marcelo <mmmarceloo@yahoo.com.br>
2022-01-30 17:24:36 -0300

    primeiro commit

---
+++ lab-padroes-projeto-spring/src/main/java/one/model/Cliente.java
@@ -0,0 +1,42 @@
+package one.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    @ManyToOne
+    private Endereco endereco;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
